fb968ba753a97fca17d7f43bacba38626860ed58
VitalikPetrakov <vitalik.petrakov@gmail.com>
2019-08-12 19:06:42 +0300

    1

---
+++ lesson2_3/lesson2_3.py
@@ -0,0 +1,16 @@
+"""TODO
+Написать декоратор - логгер. Он записывает в файл дату и время вызова функции, имя функции, аргументы, с которыми вызвалась и возвращаемое значение.
+Написать декоратора из п.1 но с параметром – пути к логам.
+Применить написанный логгер к приложению из любого предыдущего д/з.
+"""
+
+from datetime import datetime
+
+
+def logger(old_func):
+    def new_func(*args, **kwargs):
+        with open('log.txt', 'a', encoding='utf8') as file:
+            result = old_func(*args, **kwargs)
+            file.write(f'{datetime.now()} {old_func.__name__}{str(args)} {str(kwargs)}\n {result}\n')
+        return result
+    return new_func()
