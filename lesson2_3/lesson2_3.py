0cad14a2fc076a7cfb7cf26f584441611edf6391
VitalikPetrakov <vitalik.petrakov@gmail.com>
2019-08-12 20:12:07 +0300

    1

--- lesson2_3/lesson2_3.py
+++ lesson2_3/lesson2_3.py
@@ -7,10 +7,12 @@
 from datetime import datetime
 
 
-def logger(old_func):
-    def new_func(*args, **kwargs):
-        with open('log3.txt', 'a', encoding='utf8') as file:
-            result = old_func(*args, **kwargs)
-            file.write(f'{datetime.now()} {old_func.__name__}{str(args)} {str(kwargs)}\n {result}\n')
-        return result
-    return new_func()
+def log_with_path(path):
+    def logger(old_func):
+        def new_func(*args, **kwargs):
+            with open(path, 'a', encoding='utf8') as file:
+                result = old_func(*args, **kwargs)
+                file.write(f'{datetime.now()} {old_func.__name__} !{str(args)} !!{str(kwargs)}\n {result}\n')
+            return result
+        return new_func
+    return logger
