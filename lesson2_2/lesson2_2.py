4583323e3c690bc4d67555eff097f48eb3cd5248
VitalikPetrakov <vitalik.petrakov@gmail.com>
2019-08-24 16:22:00 +0300

    готовый вариант

--- lesson2_2/lesson2_2.py
+++ lesson2_2/lesson2_2.py
@@ -8,24 +8,6 @@ import json
 import hashlib
 
 
-# def read_file():
-#     with open('countries.json', 'rt', encoding='utf8') as file:
-#         data_in_file = json.load(file)
-#     return data_in_file
-#
-# print(len(read_file()))
-
-# def get_name_of_country(data_in_file):
-#     for item in data_in_file:
-#         name_of_country = item.get('name').get('common')
-#         yield name_of_country
-
-
-# def file_writer(path):
-#     with open(path, 'w', encoding='utf8') as file:
-#         for name in get_name_of_country(read_file()):
-#             file.write(f'{name} - https://en.wikipedia.org/wiki/{name.replace(" ", "_")}\n')
-
 class NameIter:
     def __init__(self):
         self.file_to_read = open('countries.json', 'rt', encoding='utf8')
@@ -54,13 +36,7 @@ def get_hash(path):
     for line in file:
         line_hash = hashlib.md5(str(line).encode("utf-8")).hexdigest()
         yield line_hash
-#
-#
-# if __name__ == '__main__':
-#     path_of_file = 'list_of_url.txt'
-#     file_writer(path_of_file)
-#     for i in get_hash(path_of_file):
-#         print(i)
+
 
 if __name__ == '__main__':
     my_class = NameIter()
