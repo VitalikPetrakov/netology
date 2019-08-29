895240761ab00b531685ad33aef0e541787b0c94
VitalikPetrakov <vitalik.petrakov@gmail.com>
2019-08-29 21:54:16 +0300

    29.08.2019

--- lesson2_5/lesson2_5.py
+++ lesson2_5/lesson2_5.py
@@ -8,6 +8,7 @@
 import email
 import smtplib
 import imaplib
+import pprint
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -99,7 +100,7 @@ class SendMailer:
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
         result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-        raw_email = data[0][1]
+        raw_email = str(data[0][1])
         email_message = email.message_from_string(raw_email)
         print(email_message)
         mail.logout()
