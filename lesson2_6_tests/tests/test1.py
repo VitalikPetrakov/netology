c890d04d91a583202bab96da8e9047304846b196
VitalikPetrakov <vitalik.petrakov@gmail.com>
2019-09-01 22:13:13 +0300

    01.09.2019

--- lesson2_6_tests/tests/test1.py
+++ lesson2_6_tests/tests/test1.py
@@ -1,13 +1,14 @@
 import unittest
 from lesson2_6_tests.secretar import check_document_existance
 import os
-
+import json
 
 
 class MyTestCase(unittest.TestCase):
     """Тест для функции check_document_existance"""
 
     def test_check_document_existance(self):
+        self.documents = json.load(out_docs)
         resalt_cheking = check_document_existance("10006")
         self.assertEqual(resalt_cheking, True)
 
@@ -16,5 +17,9 @@ if __name__ == '__main__':
     current_path = str(os.path.dirname(os.path.abspath(__file__)))
     f_directories = os.path.join(current_path, 'fixtures/directories.json')
     f_documents = os.path.join(current_path, 'fixtures/documents.json')
+    with open(f_documents, 'r') as out_docs:
+        documents = json.load(out_docs)
+    with open(f_directories, 'r') as out_dirs:
+        directories = json.load(out_dirs)
     unittest.main()
 
