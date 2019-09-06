7a38f1fc0bbd89e11d8e2e24ace3304649d7076f
VitalikPetrakov <vitalik.petrakov@gmail.com>
2019-09-06 21:11:27 +0300

    06.09.2019

--- lesson2_5/lesson2_5.py
+++ lesson2_5/lesson2_5.py
@@ -1,69 +1,13 @@
-"""Создать класс для работы с почтой;
-Создать методы для отправки и получения писем;
-Убрать "захардкоженный" код. Все значения должны определяться как аттрибуты класса, либо аргументы методов;
-Переменные должны быть названы по стандарту PEP8;
-Весь остальной код должен соответствовать стандарту PEP8;
-Класс должен инициализироваться в конструкции. if __name__ == '__main__'"""
-
 import email
 import smtplib
 import imaplib
-import pprint
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-# GMAIL_SMTP = "smtp.gmail.com"
-# GMAIL_IMAP = "imap.gmail.com"
-#
-# l = 'vitalik.petrakov123123@gmail.com'
-# passwORD = 'Vp65852341'
-# subject = 'Subject'
-# recipients = ['vitalik.petrakov@mail.ru', 'vitalik.petrakov@gmail.com']
-# message = 'Test message'
-# header = None
-#
-#
-# #send message
-# msg = MIMEMultipart()
-# msg['From'] = l
-# msg['To'] = ', '.join(recipients)
-# msg['Subject'] = subject
-# msg.attach(MIMEText(message))
-#
-# ms = smtplib.SMTP(GMAIL_SMTP, 587)
-# # identify ourselves to smtp gmail client
-# ms.ehlo()
-# # secure our email with tls encryption
-# ms.starttls()
-# # re-identify ourselves as an encrypted connection
-# ms.ehlo()
-#
-# ms.login(l, passwORD)
-# ms.sendmail(l, recipients, msg.as_string())
-#
-# ms.quit()
-#send end
-
-
-#recieve
-# mail = imaplib.IMAP4_SSL(GMAIL_IMAP)
-# mail.login(l, passwORD)
-# mail.list()
-# mail.select("inbox")
-# criterion = '(HEADER Subject "%s")' % header if header else 'ALL'
-# result, data = mail.uid('search', None, criterion)
-# assert data[0], 'There are no letters with current header'
-# latest_email_uid = data[0].split()[-1]
-# result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-# raw_email = data[0][1]
-# email_message = email.message_from_string(raw_email)
-# mail.logout()
-#end recieve
-
-
 class SendMailer:
     def __init__(self, login, password, header=None):
+        self.message = MIMEMultipart()
         self.login = login
         self.password = password
         self.gmail_smtp = "smtp.gmail.com"
@@ -75,7 +19,6 @@ class SendMailer:
         self.recipients.append(mail_address)
 
     def make_massage(self, subject, text):
-        self.message = MIMEMultipart()
         self.message['From'] = self.login
         self.message['To'] = ', '.join(self.recipients)
         self.message['Subject'] = subject
@@ -87,7 +30,8 @@ class SendMailer:
         mail_send.starttls()
         mail_send.ehlo()
         mail_send.login(self.login, self.password)
-        mail_send.sendmail(self.login, self.recipients, self.message.as_string())
+        mail_send.sendmail(self.login, self.recipients,
+                           self.message.as_string())
         mail_send.quit()
 
     def get_mail(self):
@@ -95,7 +39,10 @@ class SendMailer:
         mail.login(self.login, self.password)
         mail.list()
         mail.select("inbox")
-        criterion = '(HEADER Subject "%s")' % self.header if self.header else 'ALL'
+        if self.header:
+            criterion = f'(HEADER Subject "{self.header}")'
+        else:
+            criterion = f'(HEADER Subject "All")'
         result, data = mail.uid('search', None, criterion)
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
@@ -105,10 +52,13 @@ class SendMailer:
         print(email_message)
         mail.logout()
 
+
 if __name__ == '__main__':
-    my_sendmailer = SendMailer('vitalik.petrakov123123@gmail.com', 'Vp65852341')
+    my_sendmailer = SendMailer('vitalik.petrakov123123@gmail.com',
+                               'Vp65852341')
     my_sendmailer.add_address_to_list_recipients('vitalik.petrakov@mail.ru')
     my_sendmailer.add_address_to_list_recipients('vitalik.petrakov@gmail.com')
-    my_sendmailer.make_massage('test massage from python', 'my massage from python=)')
+    my_sendmailer.make_massage('test massage from python',
+                               'my massage from python=)')
     # my_sendmailer.send_massage()
     my_sendmailer.get_mail()
--- lesson2_6_tests/tests/test1.py
+++ lesson2_6_tests/tests/test1.py
@@ -1,73 +1,47 @@
 import unittest
-from lesson2_6_tests.secretar import check_document_existance
-import os
 import json
-import tempfile
+import lesson2_6_tests.secretar as programm
+from mock import patch
 
-class MyTestCase(unittest.TestCase):
-    """Тест для функции check_document_existance"""
+documents = []
+directories = {}
+
+
+def setUpModule():
+    with open('../fixtures/documents.json', 'r', encoding='utf-8') as out_docs:
+        documents.extend(json.load(out_docs))
+        print(documents)
+    with open('../fixtures/directories.json', 'r', encoding='utf-8') as out_dirs:
+        directories.update(json.load(out_dirs))
+        print(directories)
+
+
+@patch('lesson2_6_tests.fixtures.documents', documents, create=True)
+@patch('lesson2_6_tests.fixtures.directories', directories, create=True)
+class TestSecretaryProgram(unittest.TestCase):
 
     def setUp(self):
-        self.tempfile = tempfile.TemporaryFile()
-        # current_path = str(os.path.dirname(os.path.abspath(__file__)))
-        # f_documents = os.path.join(current_path, '../fixtures/documents.json')
-        # with open(f_documents, 'r') as self.out_docs:
-        #     self.documents = json.load(self.out_docs)
+        self.example_set = {
+            'shelf': 33,
+            'doc': 22
+        }
 
-    def tearDown(self):
-        self.tempfile.close()
+    def test_get_all_doc_owners_names(self):
+        self.assertIsInstance(programm.get_all_doc_owners_names(), set)
+        self.assertGreater(len(programm.get_all_doc_owners_names()), 0)
 
-    def test_check_document_existance(self):
+    def test_append_doc_to_shelf(self):
+        programm.append_doc_to_shelf(self.example_set['doc'], self.example_set['shelf'])
+        self.assertIn(self.example_set['doc'], directories.get(self.example_set['shelf']))
 
-        self.resalt_cheking = check_document_existance("10006")
-        self.assertEqual(self.resalt_cheking, True)
+    def test_delete_doc(self):
+        self.assertTrue(programm.check_document_existance("11-2"))
+
+        with patch('programm.input', return_value="11-2"):
+            programm.delete_doc()
+
+        self.assertFalse(programm.check_document_existance("11-2"))
 
 
 if __name__ == '__main__':
     unittest.main()
-
-# import unittest
-# import json
-# import app
-# from mock import patch
-#
-# documents = []
-# directories = {}
-#
-#
-# def setUpModule():
-#     with open('fixtures/documents.json', 'r', encoding='utf-8') as out_docs:
-#         documents.extend(json.load(out_docs))
-#     with open('fixtures/directories.json', 'r', encoding='utf-8') as out_dirs:
-#         directories.update(json.load(out_dirs))
-#
-#
-# @patch('app.documents', documents, create=True)
-# @patch('app.directories', directories, create=True)
-# class TestSecretaryProgram(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.example_set = {
-#             'shelf': 33,
-#             'doc': 22
-#         }
-#
-#     def test_get_all_doc_owners_names(self):
-#         self.assertIsInstance(app.get_all_doc_owners_names(), set)
-#         self.assertGreater(len(app.get_all_doc_owners_names()), 0)
-#
-#     def test_append_doc_to_shelf(self):
-#         app.append_doc_to_shelf(self.example_set['doc'], self.example_set['shelf'])
-#         self.assertIn(self.example_set['doc'], directories.get(self.example_set['shelf']))
-#
-#     def test_delete_doc(self):
-#         self.assertTrue(app.check_document_existance("11-2"))  # check before deleting doc
-#
-#         with patch('app.input', return_value="11-2") as child1:
-#             app.delete_doc()
-#
-#         self.assertFalse(app.check_document_existance("11-2"))  # check after deleting doc
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
