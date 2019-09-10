3f11084600591f6886983b318c8383dc3012cce7
VitalikPetrakov <vitalik.petrakov@gmail.com>
2019-09-10 22:27:12 +0300

    10.09.2019

--- lesson2_8_SQL/lesson2_8.py
+++ lesson2_8_SQL/lesson2_8.py
@@ -12,23 +12,35 @@ Course:
  """
 
 
+# for row in curs:
+#     print(row)
+
 def create_db(): # создает таблицы
-    with psycopg2.connect('dbname=netology user=netology') as conn:
+    with psycopg2.connect(dbname='netology', user='netology', password='123') as conn:
         with conn.cursor() as curs:
             curs.execute("""CREATE TABLE Student (
-                id serial PRIMARY KEY is not NULL,
-                name varchar(100) is not NULL,
+                id serial PRIMARY KEY not NULL,
+                name varchar(100) not NULL,
                 gpa numeric(10, 2),
                 birth timestamp with time zone);
                 """)
             curs.execute("""CREATE TABLE Course (
-                id serial PRIMARY KEY is not NULL,
-                name varchar(100) is not NULL);
+                id serial PRIMARY KEY not NULL,
+                name varchar(100) not NULL);
                 """)
 
 
+def del_all_tables():
+    with psycopg2.connect(dbname='netology', user='netology', password='123') as conn:
+        with conn.cursor() as curs:
+            curs.execute('DROP TABLE Student;')
+            curs.execute('DROP TABLE Course;')
+
+
 def get_students(course_id): # возвращает студентов определенного курса
-    pass
+    with psycopg2.connect(dbname='netology', user='netology', password='123') as conn:
+        with conn.cursor() as curs:
+            curs.execute('select ')
 
 
 def add_students(course_id, students): # создает студентов и
@@ -42,3 +54,8 @@ def add_student(student): # просто создает студента
 
 def get_student(student_id):
     pass
+
+
+if __name__ == '__main__':
+    create_db()
+    # del_all_tables()
