06453a8053bfbb67f33f8f9ef72a0bbc964bee18
VitalikPetrakov <vitalik.petrakov@gmail.com>
2020-01-23 20:52:50 +0300

    23.01.2020

--- lesson2_6_tests/transl.py
+++ lesson2_6_tests/transl.py
@@ -1,3 +1,5 @@
+import requests
+
 API_KEY = 'trnsl.1.1.20190714T173604Z.9b4e91eef1a8c4bb.58074b97e1b246001f37cc401bc3a872057ade11'
 
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
@@ -5,7 +7,7 @@ URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 def read_file(file_in):
     with open(file_in, 'rt', encoding='utf-8') as file_in:
-        text_for_transl = file_in.readlines() #.strip()
+        text_for_transl = file_in.readlines()
         if text_for_transl:
             file_in.readline()
     return text_for_transl
@@ -26,4 +28,4 @@ def translate_it(file_in, file_out, to_lang, in_lang='ru'):
         file_out.write(transl_text + '\n')
 
 
-translate_it('DE.txt', 'de1.txt', 'de', 'en')
+translate_it('text/DE.txt', 'de1.txt', 'de', 'en')
